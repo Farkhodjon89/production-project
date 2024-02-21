324ee0ae1f76deb4c8d66cc2fa1a45815a5a1299
Farhod <farhod@Air-Farhod.napm.local>
2024-02-21 12:38:03 +0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { render } from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
+
+render(
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById("root")
+);
