65d93a59f9de74e144b521cdadb2ce98a220a041
Farhod <farhod@Air-Farhod.napm.local>
2024-04-03 15:50:29 +0500

    fix tests

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+// import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 
 interface NavbarProps {
   className?: string
